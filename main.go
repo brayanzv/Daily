d97331dc65939465325ef6f5eac3ccc23be35a9c
brayan <brayanzuritavargas@gmail.com>
2021-04-29 16:06:07 -0400

    segundo comit

---
+++ main.go
@@ -0,0 +1,16 @@
+package main
+
+import (
+	"log"
+
+	"github.com\brayanzv\Daily\bd"
+	"github.com\brayanzv\Daily\handlers"
+)
+
+func main() {
+	if bd.CheckConnection() == 0 {
+		log.Fatal("Sin Conexion a la bd")
+		return
+	}
+	handlers.Manejadores()
+}
