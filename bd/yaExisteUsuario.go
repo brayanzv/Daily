8cbbf06953fd370e46b953e2bd84d0353daa84c3
brayan <brayanzuritavargas@gmail.com>
2021-05-03 19:57:40 -0400

    make it betters

--- bd/yaExisteUsuario.go
+++ bd/yaExisteUsuario.go
@@ -14,7 +14,7 @@ func ChequeoYaExisteUsuario(email string) (models.Usuario, bool, string) {
 	defer cancel()
 
 	db := MongoCN.Database("Daily")
-	col := db.Collection("usuarios")
+	col := db.Collection("users")
 
 	condicion := bson.M{"email": email}
 
