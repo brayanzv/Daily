46f720f37a9015cd07988cbe67269a3b5488d3f5
brayan <brayanzuritavargas@gmail.com>
2021-05-10 19:24:49 -0400

    leo Twets

---
+++ models/devuelvoTweet.go
@@ -0,0 +1,14 @@
+package models
+
+import (
+	"time"
+
+	"go.mongodb.org/mongo-driver/bson/primitive"
+)
+
+type DevueloTweet struct {
+	ID      primitive.ObjectID `bson: "_id" json: "_id,omitempty"`
+	UserId  string             `bson: "userID" json: "userId,omitempty"`
+	Mensaje string             `bson: "mensaje" json: "mensaje,omitempty"`
+	fecha   time.Time          `bson: "fecha" json: "fecha,omitempty"`
+}
